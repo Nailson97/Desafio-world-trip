72f36df596e67897165b34552c3bb22416dd82f9
Nailson97 <nailsonsousa97@gmail.com>
2023-04-13 14:44:57 -0300

    creating header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
-import  { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
