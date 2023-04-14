3c1b42925074f75572d44a34cfb7c12d617baf94
Nailson97 <nailsonsousa97@gmail.com>
2023-04-14 10:09:23 -0300

    creating banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Flex } from "@chakra-ui/react";
 import Header from "../components/Header";
+import Banner from '../components/Banner'
 
 export default function Home() {
     return (
       <Flex direction='column'>
         <Header />
+        <Banner />
       </Flex>
     )
 }
