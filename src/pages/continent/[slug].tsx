cac64ca9940b54f14038069d955adadb2c73d7bd
Nailson97 <nailsonsousa97@gmail.com>
2023-05-02 16:34:18 -0300

    Starting Continent Page

---
+++ src/pages/continent/[slug].tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+import Header from "../../components/Header";
+
+export default function Component() {
+  return (
+    <Flex direction="column">
+      <Header />
+    </Flex>
+  );
+}
