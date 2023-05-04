0732dc04d13be310cb01e56965c0bfea7fa53bb8
Nailson97 <nailsonsousa97@gmail.com>
2023-05-04 16:53:35 -0300

    Creating informative content from the Content page

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,12 +1,17 @@
 import { Flex } from "@chakra-ui/react";
 import Header from "../../components/Header";
 import ContinentBanner from "../../components/ContinentBanner";
+import Content from "../../components/Content";
 
 export default function Component() {
   return (
     <Flex direction="column">
       <Header />
       <ContinentBanner />
+
+      <Flex direction='column' maxW='1160px' mx='auto' mb='10' px='1rem'>
+        <Content />
+      </Flex>
     </Flex>
   );
 }
