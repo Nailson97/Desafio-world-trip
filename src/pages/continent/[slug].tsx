c255ef347e6eaa4d367e3b95a84a63caf354e9f9
Nailson97 <nailsonsousa97@gmail.com>
2023-05-04 11:11:15 -0300

    Finished Banner Page of the Continent

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,10 +1,12 @@
 import { Flex } from "@chakra-ui/react";
 import Header from "../../components/Header";
+import ContinentBanner from "../../components/ContinentBanner";
 
 export default function Component() {
   return (
     <Flex direction="column">
       <Header />
+      <ContinentBanner />
     </Flex>
   );
 }
