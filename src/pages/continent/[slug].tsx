44ca70190def07f2cf79aa9b047e894fe18f4d9d
Nailson97 <nailsonsousa97@gmail.com>
2023-05-06 18:38:31 -0300

    Challenge completed

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -2,6 +2,8 @@ import { Flex } from "@chakra-ui/react";
 import Header from "../../components/Header";
 import ContinentBanner from "../../components/ContinentBanner";
 import Content from "../../components/Content";
+import Cities from "../../components/Cities";
+
 
 export default function Component() {
   return (
@@ -11,6 +13,7 @@ export default function Component() {
 
       <Flex direction='column' maxW='1160px' mx='auto' mb='10' px='1rem'>
         <Content />
+        <Cities />
       </Flex>
     </Flex>
   );
